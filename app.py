75801600a7e00c424f9220c9f84f93b4484349b8
vijay singh <vijay.singh@gslab.com>
2021-02-11 14:06:33 +0530

    added pune data - v2

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-import pickle
-import joblib
+import os
+import re
 import streamlit as st
-import pandas as pd
+
+code = """<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-611FPFGKT5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-611FPFGKT5');
+</script>"""
+
+
+a = os.path.dirname(st.__file__)+'/static/index.html'
+with open(a, 'r') as f:
+    data = f.read()
+    if len(re.findall('UA-', data)) == 0:
+        with open(a, 'w') as ff:
+            newdata = re.sub('<head>', '<head>'+code, data)
+            ff.write(newdata)
+
 
 from utils import perform_match_wrapper, visualize_venue_match_results_wrapper, generate_ui_df, \
     LIST_CITY_DATA_FILE_NAME, read_data_file, get_common_feature_list, LIST_CITY, col_grain, colList_meta
